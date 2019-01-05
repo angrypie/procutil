f89c2bbcbf9f2829b90eefcce264172719f7c369
angrypie <cookies.in.chocolate@gmail.com>
2019-01-05 13:48:32 +0200

    init

---
+++ procutil.go
@@ -0,0 +1,32 @@
+package procutil
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+//DefaultTerminateDuaration is timout duration before sending SIGKILL.
+const DefaultTerminateDuaration = time.Second * 2
+
+//Terminate stops process with SIGTERM or with SIGKILL while timeout exceeded.
+func Terminate(proc *os.Process, timeout ...time.Duration) error {
+	duration := DefaultTerminateDuaration
+	if len(timeout) > 0 {
+		duration = timeout[0]
+	}
+	done := make(chan error, 1)
+	go func() {
+		done <- proc.Signal(syscall.SIGTERM)
+	}()
+	select {
+	case <-time.After(duration):
+		return proc.Kill()
+	case err := <-done:
+		if err != nil {
+			return proc.Kill()
+		}
+	}
+
+	return nil
+}
